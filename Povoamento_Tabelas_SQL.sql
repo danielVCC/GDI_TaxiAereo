630fb5c138145c9bae0065bcdca82f11e9315ec0
HOBS <43381709+HenriqueSakane@users.noreply.github.com>
2022-04-05 16:28:14 -0300

    Update Povoamento_Tabelas_SQL.sql

--- Povoamento_Tabelas_SQL.sql
+++ Povoamento_Tabelas_SQL.sql
@@ -78,6 +78,7 @@ INSERT INTO aeronave (id_aeronave,modelo) VALUES (00010,'Embraer E-195');
 INSERT INTO aeronave (id_aeronave,modelo) VALUES (10022,'Gulfstream G500');
 INSERT INTO aeronave (id_aeronave,modelo) VALUES (20333,'Bombardier Global');
 INSERT INTO aeronave (id_aeronave,modelo) VALUES (30414,'Dassault');
+INSERT INTO aeronave (id_aeronave,modelo) VALUES (30421,'Dassault');
 INSERT INTO aeronave (id_aeronave,modelo) VALUES (40525,'Pilatus PC-24');
 INSERT INTO aeronave (id_aeronave,modelo) VALUES (51434,'Phenom 300');
 
