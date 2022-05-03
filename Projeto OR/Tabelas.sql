f07d21add35fa96b10660ffabd7ee9da9f14596b
Evaldo Garcia de Souza Júnior <evalldojn@gmail.com>
2022-05-02 22:05:31 -0300

    Update Tabelas.sql

--- Projeto OR/Tabelas.sql
+++ Projeto OR/Tabelas.sql
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 DROP TABLE pessoa;
 DROP TABLE telefone;
 DROP TABLE funcionario;
@@ -22,32 +16,17 @@ DROP TABLE passagem;
 DROP TABLE gera;
 
 -- ✅ --
-CREATE TABLE pessoa(
-    cpf VARCHAR2(15) NOT NULL,
-    nome VARCHAR2(15) NOT NULL,
-    cep VARCHAR2(12) NOT NULL, 
-    logradouro VARCHAR2(40) NOT NULL, 
-    numero NUMBER NOT NULL, 
-    complemento VARCHAR2(20) NOT NULL,
-    bairro VARCHAR2(20) NOT NULL,
-    estado VARCHAR2(20) NOT NULL,
-    cidade VARCHAR2(20) NOT NULL
-);
-
--- ✅ --
-CREATE TABLE telefone(
-    cpf_pessoa_telefone VARCHAR(15) NOT NULL,
-    ddd NUMBER NOT NULL,
-    numero NUMBER NOT NULL,
-    CONSTRAINT telefone_pk PRIMARY KEY (cpf_pessoa_telefone),
-    CONSTRAINT telefone_cpf_pessoa_fk FOREIGN KEY (cpf_pessoa_telefone) REFERENCES pessoa(cpf)
+CREATE TABLE tb_telefone OF tp_telefone(
+    cpf PRIMARY KEY,
+    ddd NOT NULL,
+    numero NOT NULL
 );
 
 -- ✅ --
 CREATE TABLE tb_funcionario OF tp_funcionario(
     cpf PRIMARY KEY,
     turno_de_trabalho NOT NULL,
-    salario NOT NULL,
+    salario NOT NULL CHECK (salario >= 1100.00),
 );
 
 -- ✅ --
@@ -56,117 +35,117 @@ CREATE TABLE tb_cliente of tp_cliente(
     data_cadastro NOT NULL
 );
 
-CREATE TABLE cartao_de_milhas(
-    data_de_emissao DATE NOT NULL,
-    numero_de_milhas NUMBER NOT NULL,
-    cpf_cliente_cm VARCHAR2(15) NOT NULL, 
-    CONSTRAINT cartao_fidelidade_pk PRIMARY KEY (cpf_cliente_cm),
-    CONSTRAINT cartao_cpf_cliente_fk FOREIGN KEY (cpf_cliente_cm) REFERENCES cliente(cpf_cliente)
+-- ✅ --
+CREATE TABLE tb_cartao_de_milhas OF tp_cartao_de_milhas(
+    cpf PRIMARY KEY, 
+    data_de_emissao NOT NULL,
+    numero_de_milhas NOT NULL,
 );
 
 -- ✅ --
-CREATE TABLE atendente(
-    cpf_atendente VARCHAR2(15) NOT NULL,
-    clientes_atendidos NUMBER NOT NULL,
-    CONSTRAINT atendente_pk PRIMARY KEY (cpf_atendente),
-    CONSTRAINT atendente_cpf_funcionario_fk FOREIGN KEY (cpf_atendente) REFERENCES funcionario(cpf_funcionario)
+CREATE TABLE tb_atendente OF tp_atendente(
+    cpf PRIMARY KEY,
+    clientes_atendidos NOT NULL
 );
 
 -- ✅ --
-CREATE TABLE piloto(
-    cpf_piloto VARCHAR2(15) NOT NULL,
-    cpf_copiloto VARCHAR2(15),
-    horas_de_voo NUMBER NOT NULL,
-    CONSTRAINT piloto_pk PRIMARY KEY (cpf_piloto),
-    CONSTRAINT piloto_cpf_atendente_fk FOREIGN KEY (cpf_piloto) REFERENCES funcionario (cpf_funcionario),
-    CONSTRAINT piloto_cpf_copiloto_fk FOREIGN KEY (cpf_copiloto) REFERENCES piloto (cpf_piloto)
-);
-
-CREATE TABLE modelo_aeronave(
-    tipo_aeronave VARCHAR2(20) NOT NULL,
-    passageiros NUMBER NOT NULL,
-    carga NUMBER NOT NULL,
-    velocidade NUMBER NOT NULL,
-    categoria_Velociade VARCHAR2(20),
-    CONSTRAINT modelo_aeronave_pk PRIMARY KEY (tipo_aeronave)
-);
-
-CREATE TABLE aeronave(
-    id_aeronave NUMBER NOT NULL,
-    modelo VARCHAR2(20) NOT NULL,
-    CONSTRAINT aeronave_pk PRIMARY KEY (id_aeronave),
-    CONSTRAINT aeronave_modelo_fk FOREIGN KEY (modelo) REFERENCES modelo_aeronave(tipo_aeronave)
-);
-
-CREATE TABLE hangar(
-    id_hangar NUMBER NOT NULL,
-    capacidade NUMBER NOT NULL,
-    CONSTRAINT hangar_pk PRIMARY KEY (id_hangar)
+CREATE TABLE tb_piloto OF tp_piloto(
+    cpf PRIMARY KEY,
+    cpf_copiloto WITH ROWID REFERENCES tb_piloto,
+    horas_de_voo NOT NULL
 );
 
 -- ✅ --
-CREATE TABLE promocao(
-    codigo_promocional NUMBER NOT NULL,
-    data_de_termino DATE NOT NULL,
-    desconto NUMBER NOT NULL,
-    CONSTRAINT promocao_pk PRIMARY KEY (codigo_promocional)
+CREATE TABLE tb_modelo_aeronave OF tp_modelo_aeronave(
+    tipo_aeronave PRIMARY KEY,
+    passageiros NOT NULL,
+    carga NOT NULL,
+    velocidade NOT NULL,
+    categoria_Velociade NOT NULL
 );
 
-CREATE TABLE agendamento(
-    cpf_cliente_agendamento VARCHAR2(15) NOT NULL,
-    cpf_atendente_agendamento VARCHAR2(15) NOT NULL,
-    hora VARCHAR2(10) NOT NULL,
-    id_agendamento NUMBER NOT NULL,
-    data_agendamento DATE NOT NULL,
-    milhas_geradas NUMBER NOT NULL,
-    CONSTRAINT agendamento_pk PRIMARY KEY (id_agendamento),
-    CONSTRAINT agendamento_cpf_cliente_fk FOREIGN KEY (cpf_cliente_agendamento) REFERENCES cliente(cpf_cliente),
-    CONSTRAINT agendamento_cpf_atendete_fk FOREIGN KEY (cpf_atendente_agendamento) REFERENCES atendente(cpf_atendente)
+-- ✅ --
+CREATE TABLE tb_aeronave OF tp_aeronave(
+    id_aeronave PRIMARY KEY,
+    modelo WITH ROWID REFERENCES tb_modelo_aeronave NOT NULL
 );
 
-CREATE TABLE passagem(
-    id_passagem NUMBER NOT NULL,
-    origem VARCHAR2(20) NOT NULL,
-    destino VARCHAR2(20) NOT NULL,
-    data_de_ida DATE NOT NULL,
-    horario_passagem VARCHAR2(10) NOT NULL,
-    data_de_volta DATE NOT NULL,
-    CONSTRAINT passagem_pk PRIMARY KEY (id_passagem)
+-- ✅ --
+CREATE TABLE tb_hangar OF tp_hangar(
+    id_hangar PRIMARY KEY,
+    capacidade NOT NULL
 );
 
-CREATE TABLE realiza(
-    cpf_cliente_realiza_promo VARCHAR2(15) NOT NULL,
-    codigo_promocional_realiza NUMBER NOT NULL,
-    id_agendamento_realiza NUMBER NOT NULL,
-    CONSTRAINT realiza_tb1_pk PRIMARY KEY (cpf_cliente_realiza_promo, codigo_promocional_realiza, id_agendamento_realiza),
-    CONSTRAINT codigo_pomocional_fk FOREIGN KEY (codigo_promocional_realiza) REFERENCES promocao(codigo_promocional),
-    CONSTRAINT id_agendamento_realiza_fk FOREIGN KEY (id_agendamento_realiza) REFERENCES agendamento(id_agendamento),
-    CONSTRAINT realiza_cpf_cliente_fk FOREIGN KEY (cpf_cliente_realiza_promo) REFERENCES cliente(cpf_cliente)
+-- ✅ --
+CREATE TABLE tb_promocao OF tp_promocao(
+    codigo_promocional PRIMARY KEY,
+    data_de_termino NOT NULL,
+    desconto NOT NULL
 );
 
-CREATE TABLE estaciona(
-    cod_aeronave NUMBER NOT NULL,
-    cod_hangar NUMBER NOT NULL,
-    CONSTRAINT estaciona1_pk PRIMARY KEY (cod_aeronave,cod_hangar),
-    CONSTRAINT cod_aeronave_fk FOREIGN KEY (cod_aeronave) REFERENCES aeronave(id_aeronave),
-    CONSTRAINT cod_hangar_fk FOREIGN KEY (cod_hangar) REFERENCES hangar(id_hangar)
+-- ✅ --
+CREATE TABLE tb_agendamento OF tp_agendamento(
+    cpf_cliente_agendamento WITH ROWID REFERENCES tb_cliente NOT NULL,
+    cpf_atendente_agendamento WITH ROWID REFERENCES tb_atendente NOT NULL,
+    hora NOT NULL,
+    id_agendamento PRIMARY KEYL,
+    data_agendamento NOT NULL,
+    milhas_geradas NOT NULL
 );
 
-CREATE TABLE gera (
-  id_gera NUMBER NOT NULL,
-  data_gera DATE NOT NULL,
-  hora_gera VARCHAR2(15) NOT NULL,
-  CONSTRAINT gera_pk PRIMARY KEY (id_gera)
+-- ✅ --
+CREATE TABLE tb_passagem OF tp_passagem(
+    id_passagem PRIMARY KEY,
+    origem NOT NULL,
+    destino NOT NULL,
+    data_de_ida NOT NULL,
+    horario_passagem NOT NULL,
+    data_de_volta NOT NULL
 );
 
-ALTER TABLE funcionario ADD (
-    CONSTRAINT funcionario_salario_check CHECK (salario >= 1100.00)
+--⚠️--
+CREATE TABLE tb_realiza OF tp_realiza(
+    cpf_cliente_realiza_promo PRIMARY KEY, --⚠️ WITH ROWID REFERENCES tb_cliente NOT NULL ?--
+    codigo_promocional_realiza SCOPE IS tb_promocao,
+    id_agendamento_realiza WITH ROWID REFERENCES tb_agendamento NOT NULL
 );
 
-CREATE TABLE log_table (
-    info VARCHAR2(40)
+--⚠️--
+CREATE TABLE tb_estaciona OF tp_estaciona(
+    cod_aeronave PRIMARY KEY,  --⚠️ WITH ROWID REFERENCES tb_aeronave NOT NULL ?--
+    cod_hangar WITH ROWID REFERENCES tb_hangar NOT NULL
 );
-CREATE INDEX indice_funcionario_salario ON funcionario(salario);
-CREATE SEQUENCE seq_id_passagem INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE seq_id_agendamento INCREMENT BY 1 START WITH 1;
-CREATE SEQUENCE seq_id_realiza INCREMENT BY 1 START WITH 1;
+
+-- ✅ --
+CREATE TABLE tb_gera OF tp_gera(
+  id_gera PRIMARY KEY,
+  data_gera NOT NULL,
+  hora_gera NOT NULL
+);
+
+
+
+/* CHECKLIST
+
+1. CREATE OR REPLACE TYPE ✅
+2. CREATE OR REPLACE TYPE BODY ✅
+3. MEMBER PROCEDURE ✅
+4. MEMBER FUNCTION ✅
+5. ORDER MEMBER FUNCTION ✅
+6. MAP MEMBER FUNCTION ✅
+7. CONSTRUCTOR FUNCTION ✅
+8. OVERRIDING MEMBER ✅
+9. FINAL MEMBER ⚠️ (acho que ja foi no final map member)
+10. NOT INSTANTIABLE TYPE/MEMBER ✅
+11. HERANÇA DE TIPOS (UNDER/NOT FINAL) ✅
+12. ALTER TYPE ✅
+13. CREATE TABLE OF ✅
+14. WITH ROWID REFERENCES ✅
+15. REF ✅
+16. SCOPE IS ✅
+17. INSERT INTO 
+18. VALUE 
+19. VARRAY ✅
+20. NESTED TABLE 
+
+*/
