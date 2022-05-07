c729edf174e799899808b71bfacf678742f6f60a
HOBS <43381709+HenriqueSakane@users.noreply.github.com>
2022-05-06 23:34:33 -0300

    Update Tabelas.sql

--- Projeto OR/Tabelas.sql
+++ Projeto OR/Tabelas.sql
@@ -24,39 +24,39 @@ DROP TABLE gera;
 
 /
 
--- ⚠️ nao passou kk --
+-- ✅ --
 CREATE TABLE tb_funcionario OF tp_funcionario(
     cpf PRIMARY KEY,
     turno_de_trabalho NOT NULL,
-    salario NOT NULL CHECK (salario >= 1100.00),
+    salario NOT NULL CHECK (salario >= 1100.00)
 );
-
+/
 -- ✅ --
 CREATE TABLE tb_cliente of tp_cliente(
     cpf PRIMARY KEY,
     data_cadastro NOT NULL
 );
-
+/
 -- ⚠️ nao passou --
 CREATE TABLE tb_cartao_de_milhas OF tp_cartao_de_milhas(
     cpf PRIMARY KEY, 
     data_de_emissao NOT NULL,
-    numero_de_milhas NOT NULL,
+    numero_de_milhas NOT NULL
 );
-
--- ⚠️ nao passou --
+/
+-- ✅ --
 CREATE TABLE tb_atendente OF tp_atendente(
     cpf PRIMARY KEY,
     clientes_atendidos NOT NULL
 );
-
--- ⚠️ nao passou --
+/
+-- ✅ --
 CREATE TABLE tb_piloto OF tp_piloto(
     cpf PRIMARY KEY,
     cpf_copiloto WITH ROWID REFERENCES tb_piloto,
     horas_de_voo NOT NULL
 );
-
+/
 -- ✅ --
 CREATE TABLE tb_modelo_aeronave OF tp_modelo_aeronave(
     tipo_aeronave PRIMARY KEY,
@@ -65,36 +65,36 @@ CREATE TABLE tb_modelo_aeronave OF tp_modelo_aeronave(
     velocidade NOT NULL,
     categoria_Velociade NOT NULL
 );
-
+/
 -- ✅ --
 CREATE TABLE tb_aeronave OF tp_aeronave(
     id_aeronave PRIMARY KEY,
     modelo WITH ROWID REFERENCES tb_modelo_aeronave NOT NULL
 );
-
+/
 -- ✅ --
 CREATE TABLE tb_hangar OF tp_hangar(
     id_hangar PRIMARY KEY,
     capacidade NOT NULL
 );
-
+/
 -- ✅ --
 CREATE TABLE tb_promocao OF tp_promocao(
     codigo_promocional PRIMARY KEY,
     data_de_termino NOT NULL,
     desconto NOT NULL
 );
-
--- ⚠️ nao passou --
+/
+-- ✅ --
 CREATE TABLE tb_agendamento OF tp_agendamento(
-    cpf_cliente_agendamento WITH ROWID REFERENCES tb_cliente NOT NULL,
-    cpf_atendente_agendamento WITH ROWID REFERENCES tb_atendente NOT NULL,
+    cpf_cliente_agendamento SCOPE IS tb_cliente NOT NULL,
+    cpf_atendente_agendamento SCOPE IS tb_atendente NOT NULL,
     hora NOT NULL,
     id_agendamento PRIMARY KEY,
     data_agendamento NOT NULL,
     milhas_geradas NOT NULL
 );
-
+/
 -- ✅ --
 CREATE TABLE tb_passagem OF tp_passagem(
     id_passagem PRIMARY KEY,
@@ -104,20 +104,20 @@ CREATE TABLE tb_passagem OF tp_passagem(
     horario_passagem NOT NULL,
     data_de_volta NOT NULL
 );
-
+/
 --⚠️ nao passou --
 CREATE TABLE tb_realiza OF tp_realiza(
-    cpf_cliente_realiza_promo PRIMARY KEY, --⚠️ WITH ROWID REFERENCES tb_cliente NOT NULL ?--
+    cpf_cliente_realiza_promo PRIMARY KEY,
     codigo_promocional_realiza SCOPE IS tb_promocao,
-    id_agendamento_realiza WITH ROWID REFERENCES tb_agendamento NOT NULL
+    id_agendamento_realiza SCOPE IS tb_agendamento NOT NULL
 );
-
---⚠️ nao passou --
+/
+-- ✅ --
 CREATE TABLE tb_estaciona OF tp_estaciona(
-    cod_aeronave PRIMARY KEY,  --⚠️ WITH ROWID REFERENCES tb_aeronave NOT NULL ?--
-    cod_hangar WITH ROWID REFERENCES tb_hangar NOT NULL
+    cod_aeronave PRIMARY KEY,
+    cod_hangar SCOPE IS tb_hangar NOT NULL
 );
-
+/
 -- ✅ --
 CREATE TABLE tb_gera OF tp_gera(
   id_gera PRIMARY KEY,
