62aeef3c1c4699ce569e50c7e6a58bcedf8cb0bf
HOBS <43381709+HenriqueSakane@users.noreply.github.com>
2022-05-07 02:17:47 -0300

    Update Povoamento_OR.sql

--- Projeto OR/Povoamento_OR.sql
+++ Projeto OR/Povoamento_OR.sql
@@ -70,39 +70,39 @@ INSERT INTO tb_aeronave (id_aeronave,modelo) VALUES (51434,'Phenom 300');
 
 --hangar
 
-INSERT INTO hangar (id_hangar,capacidade) VALUES (101,4);
-INSERT INTO hangar (id_hangar,capacidade) VALUES (202,7);
+INSERT INTO tb_hangar (id_hangar,capacidade) VALUES (101,4);
+INSERT INTO tb_hangar (id_hangar,capacidade) VALUES (202,7);
 
 --promocao
 
-INSERT INTO promocao (codigo_promocional,data_de_termino,desconto) VALUES (000001,to_date('08/05/2022','dd/mm/yy'),20);
-INSERT INTO promocao (codigo_promocional,data_de_termino,desconto) VALUES (220002,to_date('27/08/2022','dd/mm/yy'),50);
-INSERT INTO promocao (codigo_promocional,data_de_termino,desconto) VALUES (330011,to_date('30/09/2022','dd/mm/yy'),10);
+INSERT INTO tb_promocao (codigo_promocional,data_de_termino,desconto) VALUES (000001,to_date('08/05/2022','dd/mm/yy'),20);
+INSERT INTO tb_promocao (codigo_promocional,data_de_termino,desconto) VALUES (220002,to_date('27/08/2022','dd/mm/yy'),50);
+INSERT INTO tb_promocao (codigo_promocional,data_de_termino,desconto) VALUES (330011,to_date('30/09/2022','dd/mm/yy'),10);
 
 --agendamento
 
-INSERT INTO agendamento (cpf_cliente_agendamento,cpf_atendente_agendamento,hora,id_agendamento,data_agendamento,milhas_geradas) VALUES ('222.333.444.57','111.222.333.44','10:43',0091,to_date('25/02/2022','dd/mm/yy'),300);
-INSERT INTO agendamento (cpf_cliente_agendamento,cpf_atendente_agendamento,hora,id_agendamento,data_agendamento,milhas_geradas) VALUES ('105.210.555.70','011.222.333.23','13:35',1235,to_date('10/03/2022','dd/mm/yy'),400);
-INSERT INTO agendamento (cpf_cliente_agendamento,cpf_atendente_agendamento,hora,id_agendamento,data_agendamento,milhas_geradas) VALUES ('777.022.333.88','011.222.333.23','16:20',1515,to_date('18/03/2022','dd/mm/yy'),600);
-INSERT INTO agendamento (cpf_cliente_agendamento,cpf_atendente_agendamento,hora,id_agendamento,data_agendamento,milhas_geradas) VALUES ('444.222.333.56','555.222.333.44','19:50',4545,to_date('31/03/2022','dd/mm/yy'),1300);
+INSERT INTO tb_agendamento (cpf_cliente_agendamento,cpf_atendente_agendamento,hora,id_agendamento,data_agendamento,milhas_geradas) VALUES ('222.333.444.57','111.222.333.44','10:43',0091,to_date('25/02/2022','dd/mm/yy'),300);
+INSERT INTO tb_agendamento (cpf_cliente_agendamento,cpf_atendente_agendamento,hora,id_agendamento,data_agendamento,milhas_geradas) VALUES ('105.210.555.70','011.222.333.23','13:35',1235,to_date('10/03/2022','dd/mm/yy'),400);
+INSERT INTO tb_agendamento (cpf_cliente_agendamento,cpf_atendente_agendamento,hora,id_agendamento,data_agendamento,milhas_geradas) VALUES ('777.022.333.88','011.222.333.23','16:20',1515,to_date('18/03/2022','dd/mm/yy'),600);
+INSERT INTO tb_agendamento (cpf_cliente_agendamento,cpf_atendente_agendamento,hora,id_agendamento,data_agendamento,milhas_geradas) VALUES ('444.222.333.56','555.222.333.44','19:50',4545,to_date('31/03/2022','dd/mm/yy'),1300);
 
 --passagem
 
-INSERT INTO passagem (id_passagem,origem,destino,data_de_ida,horario_passagem,data_de_volta) VALUES (0091,'Recife','Salvador',to_date('09/04/2022','dd/mm/yy'),'10:00',to_date('12/04/2022','dd/mm/yy'));
-INSERT INTO passagem (id_passagem,origem,destino,data_de_ida,horario_passagem,data_de_volta) VALUES (1235,'Recife','Curitiba',to_date('10/05/2022','dd/mm/yy'),'15:00',to_date('15/06/2022','dd/mm/yy'));
-INSERT INTO passagem (id_passagem,origem,destino,data_de_ida,horario_passagem,data_de_volta) VALUES (1515,'Recife','Brasilia',to_date('02/06/2022','dd/mm/yy'),'09:30',to_date('04/10/2022','dd/mm/yy'));
-INSERT INTO passagem (id_passagem,origem,destino,data_de_ida,horario_passagem,data_de_volta) VALUES (4545,'Recife','Brasilia',to_date('02/06/2022','dd/mm/yy'),'11:30',to_date('16/09/2022','dd/mm/yy'));
+INSERT INTO tb_passagem (id_passagem,origem,destino,data_de_ida,horario_passagem,data_de_volta) VALUES (0091,'Recife','Salvador',to_date('09/04/2022','dd/mm/yy'),'10:00',to_date('12/04/2022','dd/mm/yy'));
+INSERT INTO tb_passagem (id_passagem,origem,destino,data_de_ida,horario_passagem,data_de_volta) VALUES (1235,'Recife','Curitiba',to_date('10/05/2022','dd/mm/yy'),'15:00',to_date('15/06/2022','dd/mm/yy'));
+INSERT INTO tb_passagem (id_passagem,origem,destino,data_de_ida,horario_passagem,data_de_volta) VALUES (1515,'Recife','Brasilia',to_date('02/06/2022','dd/mm/yy'),'09:30',to_date('04/10/2022','dd/mm/yy'));
+INSERT INTO tb_passagem (id_passagem,origem,destino,data_de_ida,horario_passagem,data_de_volta) VALUES (4545,'Recife','Brasilia',to_date('02/06/2022','dd/mm/yy'),'11:30',to_date('16/09/2022','dd/mm/yy'));
 
 --realiza
 
-INSERT INTO realiza (cpf_cliente_realiza_promo,codigo_promocional_realiza,id_agendamento_realiza) VALUES ('222.333.444.57',000001,0091);
-INSERT INTO realiza (cpf_cliente_realiza_promo,codigo_promocional_realiza,id_agendamento_realiza) VALUES ('105.210.555.70',330011,1235);
-INSERT INTO realiza (cpf_cliente_realiza_promo,codigo_promocional_realiza,id_agendamento_realiza) VALUES ('777.022.333.88',330011,1515);
+INSERT INTO tb_realiza (cpf_cliente_realiza_promo,codigo_promocional_realiza,id_agendamento_realiza) VALUES ('222.333.444.57',000001,0091);
+INSERT INTO tb_realiza (cpf_cliente_realiza_promo,codigo_promocional_realiza,id_agendamento_realiza) VALUES ('105.210.555.70',330011,1235);
+INSERT INTO tb_realiza (cpf_cliente_realiza_promo,codigo_promocional_realiza,id_agendamento_realiza) VALUES ('777.022.333.88',330011,1515);
 
 --gera
-INSERT INTO gera (id_gera,data_gera,hora_gera) VALUES (1234,to_date('09/04/2022','dd/mm/yy'),'9:30');
-INSERT INTO gera (id_gera,data_gera,hora_gera) VALUES (4321,to_date('10/05/2022','dd/mm/yy'),'14:00');
-INSERT INTO gera (id_gera,data_gera,hora_gera) VALUES (5555,to_date('02/06/2022','dd/mm/yy'),'7:30');
+INSERT INTO tb_gera (id_gera,data_gera,hora_gera) VALUES (1234,to_date('09/04/2022','dd/mm/yy'),'9:30');
+INSERT INTO tb_gera (id_gera,data_gera,hora_gera) VALUES (4321,to_date('10/05/2022','dd/mm/yy'),'14:00');
+INSERT INTO tb_gera (id_gera,data_gera,hora_gera) VALUES (5555,to_date('02/06/2022','dd/mm/yy'),'7:30');
 
 
 --estaciona
