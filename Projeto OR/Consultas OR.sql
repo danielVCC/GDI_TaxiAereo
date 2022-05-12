3cc7444ce06500cb13ffcb17cfbd71e7bb3e241a
HOBS <43381709+HenriqueSakane@users.noreply.github.com>
2022-05-12 17:45:54 -0300

    Update Consultas OR.sql

--- Projeto OR/Consultas OR.sql
+++ Projeto OR/Consultas OR.sql
@@ -1,3 +1,7 @@
 --Selecionar a hora dos agendamentos atravez da tabela realiza
 select deref(a.id_agendamento_realiza).hora as agendamento  
 from  tb_realiza a;
+
+--Selecionar a velocidade das aeronaves presentes em modelo_aeronave atravez da tabela aeronave
+select deref(a.modelo).velocidade as aeronave  
+from  tb_modelo_aeronave a;
