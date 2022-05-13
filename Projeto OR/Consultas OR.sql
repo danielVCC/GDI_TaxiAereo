28078e0d0fb9e36af83addd6b8ae10f6f5f5fe1f
HOBS <43381709+HenriqueSakane@users.noreply.github.com>
2022-05-12 22:19:40 -0300

    Update Consultas OR.sql

--- Projeto OR/Consultas OR.sql
+++ Projeto OR/Consultas OR.sql
@@ -5,3 +5,10 @@ from  tb_realiza a;
 --Selecionar a velocidade das aeronaves presentes em modelo_aeronave atravez da tabela aeronave
 select deref(a.modelo).velocidade as aeronave  
 from  tb_aeronave a;
+
+--Selecionar os telefones, o cpf e o salario dos funcionarios que trabalham a noite
+SELECT f.telefone, 
+f.cpf, 
+f.salario
+FROM tb_funcionario f 
+WHERE turno_de_trabalho = 'Noite';
