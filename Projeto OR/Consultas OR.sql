5ee8cc7ad4af17cc1b21919875f961c16f6ce260
HOBS <43381709+HenriqueSakane@users.noreply.github.com>
2022-05-12 22:36:07 -0300

    Update Consultas OR.sql

--- Projeto OR/Consultas OR.sql
+++ Projeto OR/Consultas OR.sql
@@ -12,3 +12,10 @@ f.cpf,
 f.salario
 FROM tb_funcionario f 
 WHERE turno_de_trabalho = 'Noite';
+
+--Mude a data de agendamento de um cliente expecifico emitido em 08/10/0202 para 12/10/2020.
+UPDATE TABLE (SELECT c.data_agendamento 
+              FROM tb_agendamento c 
+              WHERE c.cpf_cliente_agendamento = (SELECT REF(f) FROM tb_cliente f WHERE f.cpf = '105.210.555.70')) cf
+    SET cf.data_agendamento = to_date('12/10/2023', 'dd/mm/yy')
+    WHERE cf.data_agendamento = to_date('10/03/2020', 'dd/mm/yy');
