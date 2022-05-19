94e683ec67d33a7a4b4d453b0953de6a072256c3
HOBS <43381709+HenriqueSakane@users.noreply.github.com>
2022-05-19 18:24:12 -0300

    Update ObraArte.js

--- MongoDB/Documents/ObraArte.js
+++ MongoDB/Documents/ObraArte.js
@@ -5,31 +5,31 @@ db.ObraArte.insertMany([
     _id: 1,
     nomeObra: "Flores ao vento",
     dataCriacao: "23/03/1850",
-    autor: [1]
+    autor: 1
   },
   {
     _id: 2,
     nomeObra: "Deserto sombrio",
     dataCriacao: "13/13/1920",
-    autor: [4]
+    autor: 4
   },
   {
     _id: 3,
     nomeObra: "Estacas",
     dataCriacao: "02/10/2010",
-    autor: [3]
+    autor: 3
   },
   {
     _id: 4,
     nomeObra: "Telhado molhado",
     dataCriacao: "23/05/1873",
-    autor: [2]
+    autor: 2
   },
   {
     _id: 5,
     nomeObra: "Ideias no papel",
     dataCriacao: "12/11/1851",
-    autor: [4]
+    autor: 4
   }
 
 ])
