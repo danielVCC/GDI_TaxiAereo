1c8855ea30219dbfa9428724b8dd659c8af7c0cd
HOBS <43381709+HenriqueSakane@users.noreply.github.com>
2022-05-19 21:14:28 -0300

    Update ObraArte.js

--- MongoDB/Documents/ObraArte.js
+++ MongoDB/Documents/ObraArte.js
@@ -1,35 +1,39 @@
 //criando a colecao de Obra de Arte
 
-db.ObraArte.insertMany([
-  {
-    _id: 1,
-    nomeObra: "Flores ao vento",
-    dataCriacao: "23/03/1850",
-    autor: 1
-  },
-  {
-    _id: 2,
-    nomeObra: "Deserto sombrio",
-    dataCriacao: "13/13/1920",
-    autor: 4
-  },
-  {
-    _id: 3,
-    nomeObra: "Estacas",
-    dataCriacao: "02/10/2010",
-    autor: 3
-  },
-  {
-    _id: 4,
-    nomeObra: "Telhado molhado",
-    dataCriacao: "23/05/1873",
-    autor: 2
-  },
-  {
-    _id: 5,
-    nomeObra: "Ideias no papel",
-    dataCriacao: "12/11/1851",
-    autor: 4
-  }
-
-])
+db.obraArte.insertMany([
+    {
+      _id: 1,
+      nomeObra: "Flores ao vento",
+      dataCriacao: "23/03/1850",
+      autor: 1,
+      museuExpo:2
+    },
+    {
+      _id: 2,
+      nomeObra: "Deserto sombrio",
+      dataCriacao: "13/13/1920",
+      autor: 4,
+      museuExpo:1
+    },
+    {
+      _id: 3,
+      nomeObra: "Estacas",
+      dataCriacao: "02/10/2010",
+      autor: 3,
+      museuExpo:3
+    },
+    {
+      _id: 4,
+      nomeObra: "Telhado molhado",
+      dataCriacao: "23/05/1873",
+      autor: 2,
+      museuExpo:5
+    },
+    {
+      _id: 5,
+      nomeObra: "Ideias no papel",
+      dataCriacao: "12/11/1851",
+      autor: 4,
+      museuExpo:4
+    }
+  ])
